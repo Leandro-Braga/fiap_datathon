3c2f10b1a17031f1ac4ab560d6f73323d9f56dfc
Leandro Braga <94468538+Leandro-Braga@users.noreply.github.com>
2025-03-10 16:20:30 -0300

    update_v2.0.1

--- frontend/blocos_st.py
+++ frontend/blocos_st.py
@@ -304,10 +304,10 @@ def init_session_state():
         
         st.divider()
 
-        st.subheader("Alunos do Grupo 38:")
+        st.subheader("Grupo 38:")
         st.write("**Leandro Braga Alves** <br> RM :orange[353057] | 3DTAT", unsafe_allow_html=True)
-        st.write("**Rodrigo Mitsuo Yoshida** <br> RM :orange[35274] | 3DTAT", unsafe_allow_html=True)
-        st.write("**Roberto Yukio Ihara** <br> RM :orange[35274] | 3DTAT", unsafe_allow_html=True)
+        # st.write("**Rodrigo Mitsuo Yoshida** <br> RM :orange[35274] | 3DTAT", unsafe_allow_html=True)
+        # st.write("**Roberto Yukio Ihara** <br> RM :orange[35274] | 3DTAT", unsafe_allow_html=True)
 
 
         st.divider()
